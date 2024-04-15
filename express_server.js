9320a596852d683a9b6f5c5416dcdd7a6c68d49b
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-15 15:31:16 -0400

    commit

--- express_server.js
+++ express_server.js
@@ -18,3 +18,14 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+const a = 1;
+app.get("/set", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
