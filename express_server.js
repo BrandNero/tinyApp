7111fce422121739f2f17a4d0df456df01bec778
Sergio rodriguez <rodriguezruizsergio@gmail.com>
2024-04-15 18:02:51 -0400

    added function for short URL

--- express_server.js
+++ express_server.js
@@ -4,12 +4,21 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-function generateRandomString() {}
+const generateRandomString = function() {
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }
+  return randomString;
+};
+
+app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -23,12 +32,12 @@ app.get("/urls", (req, res) => {
   const TemplateVars = { urls: urlDatabase};
   res.render("urls_index", TemplateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.post("/urls", (req, res) => {
   console.log(req.body);
   res.send("Ok");
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
   const TemplateVars = { id: req.params.id, longURL:"http://localhost:8080/urls/b2xVn2"};
